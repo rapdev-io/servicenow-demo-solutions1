55909564a5bbea3b28d72394639b609fc0fde669
Jon Giara <jgiara28@gmail.com>
2025-04-22 13:02:31 -0500

    Create Maths.java

---
+++ src/main/java/com/rapdev/maths/Maths.java
@@ -0,0 +1,40 @@
+package com.rapdev.maths;
+
+public class Maths 
+{
+    public int add(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c + d;
+    	return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c - d;
+    	return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c * d;
+    	return a * b;
+    }
+    
+    public int divide(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c / d;
+    	return a / b;
+    }
+    
+    public String doStuff(int a) {
+        return "hello!";
+    }
+}
